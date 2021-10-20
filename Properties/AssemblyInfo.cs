bde6cd929a05d28d0bee35f6de787abcf9282ed2
Santi871 <santivegega@gmail.com>
2021-10-20 17:23:20 -0300

    Update dependencies for NINA nightly #165 compatibility

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 
 // [MANDATORY] The assembly versioning
 //Should be incremented for each new release build of a plugin
-[assembly: AssemblyVersion("1.1.0.1")]
-[assembly: AssemblyFileVersion("1.1.0.1")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
 
 // [MANDATORY] The name of your plugin
 [assembly: AssemblyTitle("Scope 3D View")]
@@ -25,7 +25,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 
 // The minimum Version of N.I.N.A. that this plugin is compatible with
-[assembly: AssemblyMetadata("MinimumApplicationVersion", "1.11.0.1104")]
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "1.11.0.1165")]
 
 // The license your plugin code is using
 [assembly: AssemblyMetadata("License", "GPL-3.0")]
